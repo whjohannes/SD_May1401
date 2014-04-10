2fb8024ece70b0246f0d89d349bcc3dc84b66f64
Will Johannes <whjohannes@gmail.com>
2014-04-10 11:06:27 -0500

    String parser added

--- May1401_Webserver/config_parser.ino
+++ May1401_Webserver/config_parser.ino
@@ -3,26 +3,36 @@
 #include <SD.h>
 #include <string.h>
 
+String message;
 String config_array[NUM_ZONES][NUM_PROPS]; //Name, Visible, Time1, Dur1, Time2, Dur2, Time3, Dur3, Time4, Dur4
 File config_file;
 char temp_str[100];
+int comma_position;
 
 
 int split_config()
 {
-
 	config_file = SD.open("config.h");        // open web page file
     if (config_file) {
-    	while(config_file.available()) {
-			//Serial.println(config_file.readStringUntil(','));
-    	
-			for(int i = 0; i < NUM_ZONES; i++)
+    	while(config_file.available()) 
+    	{
+    		message += config_file.read();
+    	}
+		for(int i = 0; i < NUM_ZONES; i++)
+		{
+			for(int j = 0; j < NUM_PROPS; j++)
 			{
-				for(int j = 0; j < NUM_PROPS; j++)
+				//TODO: Fill config_array
+				comma_position = message.indexOf(',');
+
+				if(comma_position != -1)
 				{
-					//TODO: Fill config_array
-					config_array[i][j] = config_file.readStringUntil(',');
+      				Serial.println( message.substring(0,comma_position));
+      				message = message.substring(comma_position+1, message.length());
+      				config_array[i][j] = message;
+
 				}
+		
 			}
 		}
 	}
